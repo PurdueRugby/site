2b56a51bf7cf42982c59614129e0bdad693a2b2b
jwlehman93 <jeremylehman9@gmail.com>
2016-06-06 23:00:08 -0400

    added routes from current page

--- src/browser/routes.js
+++ src/browser/routes.js
@@ -6,6 +6,10 @@ import App from './app/App';
 const routes = (
   <Router history={hashHistory}>
     <Route path='/' component={App}>
+      <IndexRoute component={Home} />
+        <Route path='donate' component={Donate} />
+        <Route path='team' component={Team} />
+        <Route path='news' component={News} />
     </Route>
   </Router>
 );
