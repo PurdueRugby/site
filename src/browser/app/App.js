cf4564609d40844da1949f05237f9550dc58da6d
jwlehman93 <jeremylehman9@gmail.com>
2016-06-07 17:21:34 -0400

    added helmet

--- src/browser/app/App.js
+++ src/browser/app/App.js
@@ -1,6 +1,7 @@
 import React, { PropTypes, Component } from 'react';
 import Sidebar from '../common/Sidebar';
-import './App.scss';
+import Helmet from 'react-helmet';
+// import './App.scss';
 
 class App extends Component {
   constructor(props) {
@@ -8,8 +9,20 @@ class App extends Component {
   }
   render() {
     return (
-      <div className='main-container'>
-        <Sidebar />
+      <div>
+        <Helmet
+          htmlAttributes={{'lang': 'en'}}
+          title="Purdue Men's Rugby"
+          meta={[
+            {'name': 'viewport', 'content': 'width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no'}
+          ]}
+          link={[
+            {'rel': 'stylesheet', 'href': 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css'}
+          ]}
+        />
+        <div className='main-container'>
+          <Sidebar />
+        </div>
       </div>
     );
   }
--- src/browser/routes.js
+++ src/browser/routes.js
@@ -6,10 +6,10 @@ import App from './app/App';
 const routes = (
   <Router history={hashHistory}>
     <Route path='/' component={App}>
-      <IndexRoute component={Home} />
+      {/*<IndexRoute component={Home} />
         <Route path='donate' component={Donate} />
         <Route path='team' component={Team} />
-        <Route path='news' component={News} />
+        <Route path='news' component={News} />*/}
     </Route>
   </Router>
 );
